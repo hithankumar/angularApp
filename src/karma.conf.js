7e1018297590573c15cd6a87a6c2263e97088c6d
hitha <hnandaku@hithas-mbp.corp.adobe.com>
2019-02-25 12:20:51 +0530

    updating to chrome browser

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -5,7 +5,7 @@ process.env.CHROME_BIN = require('puppeteer').executablePath();
 
 module.exports = function (config) {
   config.set({
-    browsers: ['HeadlessChrome'],
+    browsers: ['Chrome'],
     browserDisconnectTimeout: 10000,
     browserDisconnectTolerance: 3,
     browserNoActivityTimeout: 60000,
