d83af048557ea39e779182465a9b755ca78ba977
hitha <hnandaku@hithas-mbp.corp.adobe.com>
2019-02-25 15:24:05 +0530

    update

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -17,15 +17,15 @@ module.exports = function (config) {
       }
     },
     browsers: ['HeadlessChrome'],
-    basePath: 'build/',
-    frameworks: ['jasmine'],
+    basePath: '',
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       //require('karma-phantomjs-launcher'),
-      // require('karma-jasmine-html-reporter'),
-      // require('karma-coverage-istanbul-reporter'),
-      // require('@angular-devkit/build-angular/plugins/karma')
+      require('karma-jasmine-html-reporter'),
+      require('karma-coverage-istanbul-reporter'),
+      require('@angular-devkit/build-angular/plugins/karma')
     ],
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
@@ -36,8 +36,7 @@ module.exports = function (config) {
       fixWebpackSourcePaths: true
     },
     browserNoActivityTimeout: 120000,
-    urlRoot: '/build/',
-    reporters: ['progress', 'kjhtml'],
+    reporters: ['progress'],
     port: 9877,
     colors: true,
     logLevel: config.LOG_INFO,
