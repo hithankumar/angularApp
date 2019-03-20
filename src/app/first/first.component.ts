64fee3c976d6f4445765abdf8c8f292ce811260b
hitha <hnandaku@hithas-mbp.corp.adobe.com>
2019-03-20 16:22:19 +0530

    test 9

--- src/app/first/first.component.ts
+++ src/app/first/first.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
 export class FirstComponent {
   title = 'my-apasdaasdasdsdp';
   constructor(){
-    console.log('asd');
+    console.log('asdasdasd');
     this.test();
     this.test1();
     this.test23();
